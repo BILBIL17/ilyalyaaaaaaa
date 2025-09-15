18a5d9239872b898e7f3177bea10e3aa4faa6bda
BILBIL17 <153350919+BILBIL17@users.noreply.github.com>
2025-09-15 09:13:37 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react,
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+
+const base =
+    mode === ' production' && process.env.GITHUB_PAGES === 'true'
+    ? '/ilyalyaaaaaaa/'
+    :'/'
+    
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
